3abb93cc243b802c42238d5c81728bb15f8e61a7
kloppstock <jonas@groeger-clan.de>
2017-01-11 20:40:13 +0100

    prettified ringbuffer code

--- src/RingBuffer.hpp
+++ src/RingBuffer.hpp
@@ -8,113 +8,103 @@
  * This class implements a generic, thread-safe and lock-free ringbuffer.
  * @author Jonas Schenke (a.k.a. kloppstock)
  */
-template <class T>
-class RingBuffer {
+template <class T> class RingBuffer {
 public:
-	/**
-	 * RingBuffer constructor
-	 * @param highest possible number of elements
-	 * @throws bad_alloc if a memory error occurs
-	 */
-	RingBuffer(size_t size)
-		: size(size) {
-	    head.store(0);
-		tail.store(0);
-		data = new T[size];
-	}
-	
-	/**
-	 * RingBuffer constructor
-	 * @param another RingBuffer object
-	 * @throws bad_alloc if a memory error occurs
-	 */
-	RingBuffer(const RingBuffer &other)
-		: size(other.size) {
-		head.store(other.head.load());
-		tail.store(other.tail.load());
-		data = new T[size];
-		memcpy(other.data, data, size * sizeof(T));
-	}
+    /**
+     * RingBuffer constructor
+     * @param highest possible number of elements
+     * @throws bad_alloc if a memory error occurs
+     */
+    RingBuffer(size_t size) : size(size)
+    {
+        head.store(0);
+        tail.store(0);
+        data = new T[size];
+    }
 
-	/**
-	 * The assignment operator was removed because it could 
-	 * cause problems in a multithreaded environment!
-	 */
-	RingBuffer& operator=(const RingBuffer& other) = delete;
+    /**
+     * RingBuffer constructor
+     * @param another RingBuffer object
+     * @throws bad_alloc if a memory error occurs
+     */
+    RingBuffer(const RingBuffer& other) : size(other.size)
+    {
+        head.store(other.head.load());
+        tail.store(other.tail.load());
+        data = new T[size];
+        memcpy(other.data, data, size * sizeof(T));
+    }
 
-	/**
-	 * RingBuffer destructor
-	 */
-	~RingBuffer() {
-		delete data;
-	}
+    /**
+     * The assignment operator was removed because it could
+     * cause problems in a multithreaded environment!
+     */
+    RingBuffer& operator=(const RingBuffer& other) = delete;
 
-	/**
-	 * Getter for size
-	 * @return size
-	 */
-	size_t getSize() const {
-		return size;
-	}
+    /**
+     * RingBuffer destructor
+     */
+    ~RingBuffer() { delete data; }
 
-	/**
-	 * Checks if the buffer is empty.
-	 * @return true if it is empty
-	 */
-	bool isEmpty() const {
-		return (head.load() == tail.load());
-	}
-	
+    /**
+     * Getter for size
+     * @return size
+     */
+    size_t getSize() const { return size; }
 
-	/**
-	 * Checks if the buffer is empty.
-	 * @return true if it is empty
-	 */
-	bool isFull() const {
-		return (increment(tail.load()) == head.load());
-	}
-	
-	/**
-	 * Tries to push an element into the RingBuffer.
-	 * @param element
-	 * @return true on success
-	 */
-	bool push(T element) {
-		size_t current_tail = tail.load();
-		if(current_tail + 1 == head.load())
-			return false;
-	    data[current_tail] = element;
-	    tail.store(increment(current_tail));
-		return true;
-	}
+    /**
+     * Checks if the buffer is empty.
+     * @return true if it is empty
+     */
+    bool isEmpty() const { return (head.load() == tail.load()); }
 
-	/**
-	 * Tries to pup an element from the RingBuffer.
-	 * @param element reference for storage
-	 * @return true on success
-	 */	
-	bool pop(T& element) {
-		size_t current_head = head.load();
-		if(current_head == tail.load())
-			return false;
-		element = data[current_head];
-	    head.store(increment(current_head));
-		return true;
-	}
+    /**
+     * Checks if the buffer is empty.
+     * @return true if it is empty
+     */
+    bool isFull() const { return (increment(tail.load()) == head.load()); }
+
+    /**
+     * Tries to push an element into the RingBuffer.
+     * @param element
+     * @return true on success
+     */
+    bool push(T element)
+    {
+        size_t current_tail = tail.load();
+        if (current_tail + 1 == head.load())
+            return false;
+        data[current_tail] = element;
+        tail.store(increment(current_tail));
+        return true;
+    }
+
+    /**
+     * Tries to pup an element from the RingBuffer.
+     * @param element reference for storage
+     * @return true on success
+     */
+    bool pop(T& element)
+    {
+        size_t current_head = head.load();
+        if (current_head == tail.load())
+            return false;
+        element = data[current_head];
+        head.store(increment(current_head));
+        return true;
+    }
 
 protected:
 private:
-	size_t size;
-	T *data;
-	std::atomic<size_t> head, tail;
+    size_t size;
+    T* data;
+    std::atomic<size_t> head, tail;
 
-	/**
-	 * Increments the given value inside the range 
-	 * of the RingBuffer size
-	 */
-	size_t increment(size_t n) const {
-		return (n + 1) % size;
-	}
+    /**
+     * Increments the given value inside the range
+     * of the RingBuffer size
+     */
+    size_t increment(size_t n) const { return (n + 1) % size; }
 };
 
 #endif //__RINGBUFFER_H__
