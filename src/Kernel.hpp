da234452a28dfdb2e2b7f8dc8e53565296efd5b2
De Narm <s3152806@msx.tu-dresden.de>
2017-01-16 17:36:42 +0100

    Fixed mapsize

--- src/Kernel.hpp
+++ src/Kernel.hpp
@@ -6,11 +6,12 @@
  *  Kernel function with conversion algorithm
  *  @param pedestal maps, gain maps, dataframes, number of frames, energy map
  */
-__global__ void calculate(uint16_t* pede, double* gain, uint16_t* data,
-                          uint16_t num, float* energy);
+__global__ void calculate(uint16_t mapsize, uint16_t* pede, double* gain,
+                          uint16_t* data, uint16_t num, float* energy);
 
 /**
  *  Kernel function to update pedestal maps, call for each level individual
  *  @param 1000+ "dark" frames, number of frames, pedestal maps
  * */
-__global__ void calibrate(uint16_t* data, uint16_t num, uint16_t* pede);
+__global__ void calibrate(uint16_t mapsize, uint16_t* data, uint16_t num,
+                          uint16_t* pede);
