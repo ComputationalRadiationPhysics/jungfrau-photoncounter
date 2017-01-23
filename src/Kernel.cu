85013e82e035555584da125d2593c426f1deb244
De Narm <s3152806@msx.tu-dresden.de>
2017-01-23 01:28:15 +0100

    Kernel now works with dynamic size

--- src/Kernel.cu
+++ src/Kernel.cu
@@ -4,18 +4,19 @@ __global__ void calculate(uint16_t mapsize, uint16_t* pede, double* gain,
                           uint16_t* data, uint16_t num, uint16_t* photon)
 {
     extern __shared__ uint16_t sPede[];
-    extern __shared__ uint16_t sGain[];
+    extern __shared__ double sGain[];
 
     uint16_t id = blockIdx.x * blockDim.x + threadIdx.x;
 
-    sPede[id] = pede[id];
-    sPede[mapsize + id] = pede[mapsize + id];
-    sPede[(mapsize * 2) + id] = pede[(mapsize * 2) + id];
-    sGain[id] = gain[id];
-    sGain[mapsize + id] = gain[mapsize + id];
-    sGain[(mapsize * 2) + id] = gain[(mapsize * 2) + id];
+    sPede[threadIdx.x] = pede[threadIdx.x];
+    sPede[blockDim.x + threadIdx.x] = pede[blockDim.x + threadIdx.x];
+    sPede[(blockDim.x * 2) + threadIdx.x] = pede[(blockDim.x * 2) + threadIdx.x];
+    sGain[threadIdx.x] = gain[threadIdx.x];
+    sGain[blockDim.x + threadIdx.x] = gain[blockDim.x + threadIdx.x];
+    sGain[(blockDim.x * 2) + threadIdx.x] = gain[(blockDim.x * 2) + threadIdx.x];
 
     __syncthreads();
+    
 
     for (int i = 0; i < num; i++) {
         uint16_t dataword = data[(mapsize * i) + id];
@@ -24,16 +25,17 @@ __global__ void calculate(uint16_t mapsize, uint16_t* pede, double* gain,
         switch ((dataword & 0xc000) >> 14) {
         case 0:
             energy =
-                (dataword & 0x3fff - sPede[id]) * sGain[id];
+                (dataword & 0x3fff - sPede[threadIdx.x]) * sGain[threadIdx.x];
             break;
         case 1:
             energy =
-                (sPede[mapsize + id] - dataword & 0x3fff) * sGain[mapsize + id];
+                (sPede[blockDim.x + threadIdx.x] - dataword & 0x3fff) * 
+                sGain[blockDim.x + threadIdx.x];
             break;
         case 3:
             energy =
-                (sPede[(2 * mapsize) + id] - dataword & 0x3fff) *
-                sGain[(2 * mapsize) + id];
+                (sPede[(2 * blockDim.x) + threadIdx.x] - dataword & 0x3fff) *
+                sGain[(2 * blockDim.x) + threadIdx.x];
             break;
         default:
             energy = 0;
@@ -47,21 +49,24 @@ __global__ void calculate(uint16_t mapsize, uint16_t* pede, double* gain,
 __global__ void calibrate(uint16_t mapsize, uint16_t* data, uint16_t num,
                           uint16_t* pede)
 {
+    extern __shared__ uint16_t sPede[];
+
     uint16_t id = blockIdx.x * blockDim.x + threadIdx.x;
 
     for (int i = 0; i < 1000; i++) {
         if (i == 0) {
-            pede[id] = data[(mapsize * i) + id] & 0x3fff;
+            sPede[threadIdx.x] = data[(mapsize * i) + id] & 0x3fff;
         }
         else {
-            pede[id] += data[(mapsize * i) + id] & 0x3fff;
+            sPede[threadIdx.x] += data[(mapsize * i) + id] & 0x3fff;
         }
     }
 
     for (int i = 1000; i < num; i++) {
-        pede[id] =
-            (pede[id] + data[(mapsize * i) + id] & 0x3fff) - (pede[id] / i);
+        sPede[threadIdx.x] =
+            (sPede[threadIdx.x] + data[(mapsize * i) + id] & 0x3fff) - 
+            (sPede[threadIdx.x] / i);
     }
 
-    pede[id] = round((double)pede[id] / 1000);
+    pede[id] = round((double)sPede[threadIdx.x] / 1000);
 }
