ebabdf421bb03d0143d7aa85f291db9acdf9fa90
De Narm <s3152806@msx.tu-dresden.de>
2017-04-28 16:36:47 +0200

    fixed header-offset; kernel

--- src/Kernel.cu
+++ src/Kernel.cu
@@ -47,8 +47,8 @@ __global__ void calculate(uint32_t mapsize, uint16_t* pede, double* gain,
         photon[(mapsize * i) + id + (8 * (i+1))] = int((energy + 6.2) / 12.4);
 		 
         if(threadIdx.x < 8) {
-            photon[(mapsize * i) + id + (threadIdx.x * (i+1))] = 
-                data[(mapsize * i) + id + (threadIdx.x * (i+1))];
+            photon[(mapsize * i) + (threadIdx.x * (i+1))] = 
+                data[(mapsize * i) + (threadIdx.x * (i+1))];
 		}
 	}
 }
